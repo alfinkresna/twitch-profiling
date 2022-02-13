ec1691139049f3de1f5dea95803550a30314d180
A l f i n d o <75003444+alfinkresna@users.noreply.github.com>
2022-02-13 15:18:51 +0700

    Add files via upload

--- twitch.rb
+++ twitch.rb
@@ -1,5 +1,16 @@
 require 'httparty'
+require 'colorize'
 
+def sys_c
+   if RUBY_PLATFORM =~ /win32|win64\.NET|windows|cygwin|mingw32/i
+      system('cls')
+   else
+      system('clear')
+  end
+ end
+ 
+ sys_c
+ 
 def b
 print"""                         
  _         _ _       _   
@@ -12,43 +23,42 @@ print"""
 
    Created by Alfin
 
-"""
+""".colorize(:color => :blue)
 
 end
 
 def main
 
-puts "> Masukan Username : "
+puts "> Masukan Username : ".colorize(:color => :red)
 puts()
 i = gets.chomp()
 r = HTTParty.get('https://api.ivr.fi/twitch/resolve/' + i)
 
 puts()
-puts "> Nama : #{r['displayName']}"
+puts "> Nama : #{r['displayName']}".colorize(:color => :blue)
 puts()
-puts "> Banned : #{r['banned']}"
+puts "> Banned : #{r['banned']}".colorize(:color => :red)
 puts()
-puts "> ID : #{r['id']}"
+puts "> ID : #{r['id']}".colorize(:color => :blue)
 puts()
-puts "> Bio : #{r['bio']}"
+puts "> Bio : #{r['bio']}".colorize(:color => :red)
 puts()
-puts "> Logo : #{r['logo']}"
+puts "> Logo : #{r['logo']}".colorize(:color => :blue)
 puts()
-puts "> Partner : #{r['partner']}"
+puts "> Partner : #{r['partner']}".colorize(:color => :red)
 puts()
-puts "> Afiliasi : #{r['affiliate']}"
+puts "> Afiliasi : #{r['affiliate']}".colorize(:color => :blue)
 puts()
-puts "> Badge >> Set ID : #{r['badge'][0]['setID']} , Title : #{r['badge'][0]['title']} , Versi : #{r['badge'][0]['version']}"
+puts "> Badge >> Set ID : #{r['badge'][0]['setID']} , Title : #{r['badge'][0]['title']} , Versi : #{r['badge'][0]['version']}".colorize(:color => :red)
 puts()
-puts "> Bot : #{r['bot']}"
+puts "> Bot : #{r['bot']}".colorize(:color => :blue)
 puts()
-puts "> Memerlukan Akun Terverifikasi : #{r['chatSettings']['requireVerifiedAccount']}"
+puts "> Memerlukan Akun Terverifikasi : #{r['chatSettings']['requireVerifiedAccount']}".colorize(:color => :red)
 puts()
-puts "> Tanggal Pembuatan Akun : #{r['createdAt']}"
+puts "> Tanggal Pembuatan Akun : #{r['createdAt']}".colorize(:color => :blue)
 puts()
-puts "> Terakhir Update : #{r['updatedAt']}"
+puts "> Terakhir Update : #{r['updatedAt']}".colorize(:color => :red)
 end
 
 b
 main
-puts()
